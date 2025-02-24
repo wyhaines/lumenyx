abd4f58667fefde430a293e038e5fb04e8a70dd5
Kirk Haines <wyhaines@gmail.com>
2025-02-24 16:48:29 -0700

    One has to start somewhere.

---
+++ spec/lumenyx_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Lumenyx do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/lumenyx.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Lumenyx`
+module Lumenyx
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
